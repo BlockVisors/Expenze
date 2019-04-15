afdf670b88611ca3331eade927b99a183aaa28ca
Dameon Green <dameongreen@gmail.com>
2019-04-15 15:53:17 -0400

    removed duymmy expense data..

--- src/app.js
+++ src/app.js
@@ -8,9 +8,13 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
+
+const store = configureStore();
 
 const jsx = (
-    <Provider>
+    <Provider store={store}>
     <AppRouter />
  
     </Provider>
